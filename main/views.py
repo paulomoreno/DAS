92cfc2ef8d16dc7bf0841adba49a72a73eae034c
moreno <moreno@blubel-ubuntu.(none)>
2014-10-23 21:49:52 -0200

    Correcao do erro ao enviar um post de qrCodeScan

--- main/views.py
+++ main/views.py
@@ -5,6 +5,23 @@
 from django.http import Http404, HttpResponseBadRequest, HttpResponse
 from django.shortcuts import *
 from polls.models import *
+import json
+
+def api_monta_json(obj_json):
+    '''
+    Função que retorna um objeto JSON
+    '''
+    return HttpResponse(json.dumps(obj_json, indent=4), content_type="application/json")
+
+def PrintException():
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, f.f_globals)
+    print 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
+
 
 # Create your views here.
 
@@ -48,17 +65,19 @@ def registrar_cliente(request):
         return HttpResponse('Método Não Permitido',status=405)
         
 def qrCodeScan(request):
-	'''
-	 qrCode scanner
-	'''
-	return render_to_response('main/qrCodeScanner/index.html')
+    '''
+    qrCode scanner
+    '''
+    context = RequestContext(request)
+    return render_to_response('main/qrCodeScanner/index.html', context)
 
 def searchCode(request):
+
     if request.method == 'POST':
-        entrada = request.POST.get('qrcode')
+        entrada = request.POST['qrCode']
+
     print(entrada)
-    print('sdsdsdsdsdsds')
-    return 1
-        # u = Usuario.objects.get(nome=request.GET.get('code',''))
-        # print(entrada)
-        #return render_to_response('https://www.google.com.br/?q=');
+    print('\n\nsdsdsdsdsdsds\n\n\n')
+
+    return api_monta_json({'response':entrada})
+
