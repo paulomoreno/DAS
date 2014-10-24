ef2c5688c5a528c3d31dcd9fcbafae9fb2d11e2c
paulomoreno <paulomorenoo@gmail.com>
2014-10-24 15:55:20 -0200

    Insercao de todos os modelos na interface de admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from main.models import *
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Medico)
+admin.site.register(Especializacao)
+admin.site.register(Horario)
+admin.site.register(Convenio)
+admin.site.register(Consulta)
+
